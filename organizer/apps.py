532624dae9b00edacf305e3b28fa8507cb92a365
walterQiu <walter85621@gmail.com>
2026-06-19 13:02:23 +0800

    feat: add other template file of the organizer app

---
+++ organizer/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class OrganizerConfig(AppConfig):
+    """Application configuration for the organizer app."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "organizer"
